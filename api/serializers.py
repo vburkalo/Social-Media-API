bd11832b3fb04008a069f612cf369f16fee80fa6
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-31 14:04:35 +0300

    added & implemented serializers.py in api directory

---
+++ api/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "email", "bio", "profile_picture")
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "password", "email")
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
