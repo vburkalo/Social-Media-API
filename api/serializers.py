dc6fff77b7d14b0b0f0d6918f86417a78582262d
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-31 14:32:32 +0300

    implemented Like & Comment serializers in api serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from api.models import Follow, Post
+from api.models import Follow, Post, Like, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -39,3 +39,15 @@ class PostSerializer(serializers.ModelSerializer):
             "media",
         )
         read_only_fields = ("user", )
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ("id", "user", "post", "created_at")
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("id", "user", "post", "content", "created_at")
