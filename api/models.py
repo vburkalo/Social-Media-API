7252d98c9ccd6a027da7f4f01054f93d71d966e0
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-31 14:00:21 +0300

    created CustomUser model in api models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures",
+        blank=True,
+        null=True,
+    )
