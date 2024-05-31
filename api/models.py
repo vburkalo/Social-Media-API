839fdcaaeb1e6b0caeae883b039fbda47def3eab
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-31 14:30:52 +0300

    implemented new Like & Comment models in api models.py

--- api/models.py
+++ api/models.py
@@ -45,3 +45,33 @@ class Post(models.Model):
 
     def __str__(self):
         return self.content[:20]
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
