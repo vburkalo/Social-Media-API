560a99a9957375df72d34a1c76ba4e55bc931b40
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-31 14:07:23 +0300

    implemented views for api app

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
+from api.serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
