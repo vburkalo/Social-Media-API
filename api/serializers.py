11bcfb6d97b6be0a318cfad094ca7c02eaae6071
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-02 15:43:54 +0300

    refactored class Meta with better practices

--- api/serializers.py
+++ api/serializers.py
@@ -10,7 +10,15 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "bio", "profile_picture", "followers_count", "following_count")
+        fields = (
+            "id",
+            "username",
+            "email",
+            "bio",
+            "profile_picture",
+            "followers_count",
+            "following_count"
+        )
 
     def get_followers_count(self, obj):
         return obj.followers.count()
