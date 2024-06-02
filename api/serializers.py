bcba74e5eb60760af131250149758e3edc281010
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-02 15:43:11 +0300

    added new following_count & followers_count MethodFields to UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -5,9 +5,18 @@ from api.models import Follow, Post, Like, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "bio", "profile_picture")
+        fields = ("id", "username", "email", "bio", "profile_picture", "followers_count", "following_count")
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
