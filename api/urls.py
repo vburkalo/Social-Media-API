b1d2180c06841ed5d86388a117ee27f027b68d46
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-02 15:37:16 +0300

    implemented logout, search, follow, unfollow, following, followers, own-posts, following posts, search-posts endpoints to api urls.py

--- api/urls.py
+++ api/urls.py
@@ -8,19 +8,37 @@ from rest_framework_simplejwt.views import (
 from api.views import (
     RegisterView,
     UserProfileView,
-    FollowView, PostViewSet, LikeView, CommentViewSet, SchedulePostView,
+    FollowView,
+    PostViewSet,
+    LikeView,
+    CommentViewSet,
+    SchedulePostView,
+    LogoutView,
+    UserSearchView,
+    FollowingListView,
+    FollowersListView,
+    UnfollowView,
+    OwnPostsListView,
+    FollowingPostsListView,
+    SearchPostsView,
 )
 
 router = DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
+router.register("comments", CommentViewSet, basename="comment")
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token"),
     path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
     path("profile/", UserProfileView.as_view(), name="profile"),
-    path("follow/<str:username>", FollowView.as_view(), name="follow"),
+    path("search/", UserSearchView.as_view(), name="user-search"),
+    path("follow/<str:username>/", FollowView.as_view(), name="follow"),
+    path("unfollow/<str:username>/", UnfollowView.as_view(), name="unfollow"),
+    path("following/", FollowingListView.as_view(), name="following-list"),
+    path("followers/", FollowersListView.as_view(), name="followers-list"),
     path("posts/<int:post_id>/like/", LikeView.as_view(), name="like"),
     path(
         "posts/<int:post_id>/comments/",
@@ -29,6 +47,9 @@ urlpatterns = [
         ),
         name="comments"),
     path("schedule_post/", SchedulePostView.as_view(), name="schedule_post"),
+    path("own-posts/", OwnPostsListView.as_view(), name="own-posts-list"),
+    path("following-posts/", FollowingPostsListView.as_view(), name="following-posts-list"),
+    path("search-posts/", SearchPostsView.as_view(), name="search-posts"),
 ]
 
 urlpatterns += router.urls
