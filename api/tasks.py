54bbf7575f6a44fb7e2cc202e4cf359c94253545
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-04 15:09:09 +0300

    deleted tasks.py

--- api/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-from django.utils import timezone
-from api.models import Post, CustomUser
-
-
-@shared_task
-def create_scheduled_post(user_id, content, media=None):
-    user = CustomUser.objects.get(id=user_id)
-    Post.objects.create(user=user, content=content, media=media)
